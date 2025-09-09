88f8f33881f442171fea7515dbfe258a11af7987
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2025-09-09 16:11:47 +0530

    Added trpc with express adapter

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import logger from './config/logger.config';
 import { attachCorrelationIdMiddleware } from './middlewares/correlation.middleware';
 import { initRedis } from './config/redis';
 import { connectDB } from './config/db';
+import { createExpressMiddleware } from '@trpc/server/adapters/express';
+import { urlRouter } from './routers/trpc/url';
 const app = express();
 
 app.use(express.json());
@@ -16,6 +18,12 @@ app.use(express.json());
  */
 
 app.use(attachCorrelationIdMiddleware);
+
+app.use('/trpc', createExpressMiddleware({
+    router: urlRouter
+}));
+
+
 app.use('/api/v1', v1Router);
 app.use('/api/v2', v2Router); 
 
